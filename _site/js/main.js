eb44a46a8f2ab1ccf17c222a3301412a649649ef
mateevd <mateevd@gmail.com>
2017-08-05 18:40:22 +0200

    back to top btn

--- _site/js/main.js
+++ _site/js/main.js
@@ -39,3 +39,19 @@ function menuClick() {
 }
 
 menuClick();
+
+// back to top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 1120 || document.documentElement.scrollTop > 1120) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
